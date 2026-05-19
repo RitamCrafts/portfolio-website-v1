ecbea2623d47c6918955971bc696d3d2498697f0
RitamCrafts <ritamrm1157@gmail.com>
2026-05-19 15:13:46 +0530

    minor-change 19052026

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -24,7 +24,7 @@ function FeaturedProjects({projects}) {
           </div>
       </div>
 
-      <button onClick={()=>setShowAll((prev)=>!prev)} class="px-6 py-3 cursor-pointer mx-auto rounded-lg font-medium transition-all flex items-center gap-2 bg-slate-800 hover:bg-slate-700 text-white mb-8">
+      <button onClick={()=>setShowAll((prev)=>!prev)} class="px-6 py-3 cursor-pointer mx-auto rounded-lg font-medium transition-all flex items-center gap-2 bg-slate-800 hover:bg-slate-700 active:bg-slate-700 text-white mb-8">
         {(!showAll)?"View All Projects":"Show Less"}
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class={`lucide lucide-chevron-down ${showAll ? "-rotate-z-180" : ""} transition-transform`}><path d="m6 9 6 6 6-6"></path></svg>
       </button>
